d84282327e0c56d080936e21116caba0bf3ce3b9
Maxime <maxiloc@gmail.com>
2018-04-19 13:58:32 +0200

    Update Airport.java

--- src/indexing-relevance/java/src/main/java/com/algolia/Airport.java
+++ src/indexing-relevance/java/src/main/java/com/algolia/Airport.java
@@ -1,5 +1,8 @@
 package com.algolia;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Airport {
 
   private String name;
