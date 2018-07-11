2dde181e688e1b6d58e26c7606c151facd496f65
Samuel Vaillant <samuel.vllnt@gmail.com>
2018-07-11 14:31:05 +0200

    fix(Facet): avoid to unmount the RefinementList when no results from SFFV (#11)

--- demos/react/ecommerce/src/Facet.js
+++ demos/react/ecommerce/src/Facet.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { RefinementList } from "react-instantsearch-dom";
-import { connectRefinementList } from "react-instantsearch/connectors";
+import { RefinementList, connectRefinementList } from "react-instantsearch-dom";
+
 export default connectRefinementList(
-  ({ translations, searchable, attribute, items }) =>
-    items.length && items.length > 0 ? (
+  ({ translations, searchable, attribute, items, isFromSearch }) =>
+    isFromSearch || (items.length && items.length > 0) ? (
       <RefinementList
         attribute={attribute}
         searchable={searchable}
