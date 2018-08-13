f00ed514112cb0c53e2a455fbffa73bb39b76320
Sarah Dayan <sarah.dayan@algolia.com>
2018-08-13 11:48:33 +0200

    Use built-in geoSearch component

--- demos/instantsearchjs/geo/src/app.js
+++ demos/instantsearchjs/geo/src/app.js
@@ -1,113 +1,121 @@
 /* global instantsearch */
 
-import * as Helpers from "./helpers";
-
 const search = instantsearch({
-  appId: "B1G2GM9NG0",
-  apiKey: "aadef574be1f9252bb48d4ea09b5cfe5",
-  indexName: "demo_geo",
+  appId: 'B1G2GM9NG0',
+  apiKey: 'aadef574be1f9252bb48d4ea09b5cfe5',
+  indexName: 'demo_geo',
   searchParameters: {
     hitsPerPage: 6,
     getRankingInfo: true,
     aroundLatLngViaIP: true,
-    typoTolerance: "min"
+    typoTolerance: 'min'
   }
-});
+})
 
 // Uncomment the following widget to add a map.
 
-/* let { map, markers, infoWindows } = Helpers.initGoogleMaps();
+const InfoWindow = new window.google.maps.InfoWindow()
 
 search.addWidget(
-  instantsearch.connectors.connectHits(HitsRenderingOptions => {
-    const hits = HitsRenderingOptions.hits;
-
-    markers = Helpers.resetMarkers(markers);
-
-    for (let i = 0; i < hits.length; ++i) {
-      const hit = hits[i];
-      const marker = new google.maps.Marker({
-        position: { lat: hit._geoloc.lat, lng: hit._geoloc.lng },
-        map: map,
-        airport_id: hit.objectID,
-        title:
-          hit.name === hit.city
-            ? `${hit.name} - ${hit.country}`
-            : `${hit.name} - ${hit.city}, ${hit.country}`
-      });
-      const infowindow = Helpers.buildInfoWindow(hit);
-
-      markers.push(marker);
-      infoWindows.push(infowindow);
-
-      marker.addListener("click", () => {
-        infoWindows.forEach(infowindow => infowindow.close());
-        infowindow.open(map, marker);
-      });
+  instantsearch.widgets.geoSearch({
+    container: '#map',
+    googleReference: window.google,
+    initialZoom: 4,
+    mapOptions: {
+      streetViewControl: false,
+      mapTypeControl: false,
+      zoom: 4,
+      minZoom: 3,
+      maxZoom: 6,
+      styles: [{ stylers: [{ hue: '#3596D2' }] }]
+    },
+    enableRefineControl: false,
+    enableRefineOnMapMove: false,
+    enableClearMapRefinement: false,
+    builtInMarker: {
+      events: {
+        click: ({ item, marker, map }) => {
+          if (InfoWindow.getMap()) InfoWindow.close()
+          InfoWindow.setContent(
+            `${item.name} - ${item.name === item.city ? '' : `${item.city}, `}${
+              item.country
+            }<br>${item.nb_airline_liaisons} liaisons`
+          )
+          InfoWindow.open(map, marker)
+        }
+      }
     }
-
-    map.fitBounds(Helpers.getMapBounds(markers));
-  })()
-); */
+  })
+)
 
 // Uncomment the following widget to add a search bar.
 
-/* search.addWidget(
+search.addWidget(
   instantsearch.widgets.searchBox({
-    container: "#searchbox",
-    placeholder: "Search airports by name, city, airport code",
+    container: '#searchbox',
+    placeholder: 'Search airports by name, city, airport code',
     autofocus: false
   })
-); */
+)
 
 // Uncomment the following widget to add hits list.
 
-/* search.addWidget(
+search.addWidget(
   instantsearch.widgets.hits({
-    container: "#hits",
+    container: '#hits',
     templates: {
-      empty: "No results.",
+      empty: 'No results.',
       item(hit) {
         return `
           <div class="hit">
             <h2 class="hit-name">
               <span class="hit-airport-name">
                 ${hit._highlightResult.name.value}
-                ${!hit.displayCity ? " " + hit._highlightResult.city.value : ""}
+                ${hit._highlightResult.city.value}
               </span>
               <span class="hit-airport-code">
                 (${hit._highlightResult.airport_id.value})
               </span>
             </h2>
             <p class="hit-location">
-              ${hit._highlightResult.country.value}
-              <span class="hit-distance">(${parseInt(
-                hit._rankingInfo.matchedGeoLocation.distance / 1000,
-                10
-              )} km away)</span>
+              ${hit._highlightResult.country.value}<br />
+              <span class="hit-distance">
+                ${
+                  hit._rankingInfo && hit._rankingInfo.matchedGeoLocation
+                    ? `<span>
+                ${parseInt(
+                  hit._rankingInfo.matchedGeoLocation.distance / 1000,
+                  10
+                )}km away,
+              </span>`
+                    : ''
+                }
+                ${hit.nb_airline_liaisons} liaisons
+              </span>
+
             </p>
           </div>
-        `;
+        `
       }
     }
   })
-); */
+)
 
 // Uncomment the following widget to add search stats.
 
-/* search.addWidget(
+search.addWidget(
   instantsearch.widgets.stats({
-    container: "#stats",
+    container: '#stats',
     templates: {
       body(hit) {
         return `<span role="img" aria-label="emoji">⚡️</span> <strong>${
           hit.nbHits
         }</strong> results found ${
-          hit.query != "" ? `for <strong>"${hit.query}"</strong>` : ``
-        } in <strong>${hit.processingTimeMS}ms</strong>`;
+          hit.query != '' ? `for <strong>"${hit.query}"</strong>` : ``
+        } in <strong>${hit.processingTimeMS}ms</strong>`
       }
     }
   })
-); */
+)
 
-search.start();
+search.start()
