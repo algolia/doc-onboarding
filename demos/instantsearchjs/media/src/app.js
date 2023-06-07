fb2e17a816ea10db34ca7dc608dfb7c9a4894ac2
Vladislav Fitc <vladislav.fitz@gmail.com>
2023-06-07 20:26:04 +0200

    media demo wip

--- demos/instantsearchjs/media/src/app.js
+++ demos/instantsearchjs/media/src/app.js
@@ -3,43 +3,46 @@
 import { hitTemplate } from "./helpers";
 
 const search = instantsearch({
-  appId: "B1G2GM9NG0",
-  apiKey: "aadef574be1f9252bb48d4ea09b5cfe5",
-  indexName: "demo_media",
-  searchParameters: {
+  searchClient: algoliasearch(
+    'B1G2GM9NG0',
+    'aadef574be1f9252bb48d4ea09b5cfe5',
+  ),
+  indexName: "demo_media"
+});
+
+search.addWidget(
+  instantsearch.widgets.configure({
     hitsPerPage: 3,
     attributesToSnippet: ["content:14"],
     snippetEllipsisText: " [...]"
-  }
-});
+  })
+);
 
 // Uncomment the following widget to add hits list.
 
-/* search.addWidget(
+search.addWidget(
   instantsearch.widgets.hits({
     container: "#hits",
     templates: {
       empty: "No results found.",
-      item(hit) {
-        return hitTemplate(hit);
-      }
+      item: hitTemplate
     }
   })
-); */
+);
 
 // Uncomment the following widget to add a search bar.
 
-/* search.addWidget(
+ search.addWidget(
   instantsearch.widgets.searchBox({
     container: "#searchbox",
     placeholder: "Search articles",
     autofocus: false
   })
-); */
+);
 
 // Uncomment the following widget to add search stats.
 
-/* search.addWidget(
+search.addWidget(
   instantsearch.widgets.stats({
     container: "#stats",
     templates: {
@@ -50,27 +53,26 @@ const search = instantsearch({
       }
     }
   })
-); */
+);
 
 // Uncomment the following widget to add categories list.
 
-/* search.addWidget(
+search.addWidget(
   instantsearch.widgets.refinementList({
     container: "#categories",
-    attributeName: "categories",
-    autoHideContainer: false,
+    attribute: "categories",
     templates: {
       header: "Categories"
     }
   })
-); */
+);
 
 // Uncomment the following widget to add pagination.
 
-/* search.addWidget(
+search.addWidget(
   instantsearch.widgets.pagination({
     container: "#pagination"
   })
-); */
+);
 
 search.start();
--- demos/instantsearchjs/media/src/helpers.js
+++ demos/instantsearchjs/media/src/helpers.js
@@ -1,42 +1,40 @@
-export function hitTemplate(hit) {
+export function hitTemplate(hit, { html, components }) {
   return `
     <div>
       <article>
-        <div class="post-img">
-          <a href="${hit.permalink}">
-          <img src="${hit.image}">
-          </a>
+      <div class="post-img">
+      <a href="${hit.permalink}">
+      <img src="${hit.image}">
+      </a>
+    </div>
+    <div class="post-content">
+      <div class="post-date">
+        ${hit.post_date_formatted}
+      </div>
+      <h2 class="entry-title">
+        <a href="${hit.permalink}" rel="bookmark">
+        ${instantsearch.highlight({ attribute: 'post_title', hit })}
+        </a>
+      </h2>
+      <div class="post-excerpt">
+        ${instantsearch.snippet({ attribute: 'content', hit })}
+      </div>
+      <div class="entry-meta clear">
+        <div class="author-gravatar">
+          <img src="${hit.author_image_url}" width="40" height="40">
         </div>
-        <div class="post-content">
-          <div class="post-date">
-            ${hit.post_date_formatted}
+        <div class="entry-author-content">
+          <div class="author-name">
+            ${instantsearch.highlight({ attribute: 'author_name', hit })}
           </div>
-          <h2 class="entry-title">
-            <a href="${hit.permalink}" rel="bookmark">
-            ${hit._highlightResult.post_title.value}
-            </a>
-          </h2>
-          <div class="post-excerpt">
-            ${hit._snippetResult.content.value}
-          </div>
-          <div class="entry-meta clear">
-            <div class="author-gravatar">
-              <img src="${hit.author_image_url}" width="40" height="40">
-            </div>
-            <div class="entry-author-content">
-              <div class="author-name">
-                ${hit._highlightResult.author_name.value}
-              </div>
-              <div class="post-meta-info">
-                ${
-                  hit.time_to_read
-                } min read in ${hit._highlightResult.categories
-    .map(x => x.value)
-    .join(", ")}
-              </div>
-            </div>
+          <div class="post-meta-info">
+            ${hit.time_to_read
+    } min read in ${instantsearch.highlight({ attribute: 'categories', hit }) }
+    <!--Not sure how to map this value to join-->
           </div>
         </div>
+      </div>
+    </div>
       </article>
     </div>`;
 }
